f8cdd67e3384bfc9248e01e616d46da5552e9d92
Eric Borczuk <eric.borczuk@datastax.com>
2021-07-29 17:53:06 -0400

    I think this is finally working, had to unescape at the last possible moment

--- restapi/src/main/java/io/stargate/web/docsapi/service/query/ExpressionParser.java
+++ restapi/src/main/java/io/stargate/web/docsapi/service/query/ExpressionParser.java
@@ -169,7 +169,6 @@ public class ExpressionParser {
         }
       }
     }
-
     return expressions;
   }
 
@@ -276,15 +275,12 @@ public class ExpressionParser {
     List<String> convertedFieldNamePath =
         Arrays.stream(fieldNamePath)
             .map(DocsApiUtils::convertArrayPath)
-            .map(DocsApiUtils::convertEscapedCharacters)
             .collect(Collectors.toList());
 
     if (!prependedPath.isEmpty()) {
       List<String> prependedConverted =
           prependedPath.stream()
-              .map(
-                  path ->
-                      DocsApiUtils.convertEscapedCharacters(DocsApiUtils.convertArrayPath(path)))
+              .map(path -> DocsApiUtils.convertArrayPath(path))
               .collect(Collectors.toList());
 
       convertedFieldNamePath.addAll(0, prependedConverted);
